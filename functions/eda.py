1293e66e3ccd38a9b7a717493e133aee7211b329
Rizqiansyah <rizqiansyah52@gmail.com>
2023-01-26 22:23:47 +0700

    Resolved possible future issuess Feedback dari kak Abiyyu: Buat fungsinya bisa nerima raw data jika ada lebih dari 2 loader

--- functions/eda.py
+++ functions/eda.py
@@ -6,16 +6,17 @@ def timeseries_viz(data, unit, date_range):
 
     Parameters:
     data (dataframe): Variable of RITASE Dataframe
-    unit (str): Unit Number. Either ex42 or ex44
+    unit (str): Unit Number. Either EX42 or EX44
     date_range (str): Date Range that will be grouped by. Either day or month
 
     Returns:
     RITASE Timeseries (visualization): The visualizaation of RITASE in day or month
     """
+    data = data.pivot(index='date', columns='loader')['ritase']
     data = data.reset_index().groupby(pd.Grouper(key='date', axis=0, freq=date_range)).sum()
     fig, ax = plt.subplots(figsize=(18, 8))
-    plt.title(f'Ritase {unit.upper()} Timeseries', fontsize=20, y=1)
+    plt.title(f'Ritase {unit} Timeseries', fontsize=20, y=1)
     plt.xlabel('Dates', fontsize=14)
-    plt.ylabel(f'Ritase {unit.upper()}', fontsize=14)
-    ax.fill_between(data.index, data[f'ritase_{unit}'], color='#96b9d0', linewidth=1.1, alpha=0.1)
-    ax.plot(data.index, data[f'ritase_{unit}'], color='#fc8d62', linewidth=1.1)
+    plt.ylabel(f'Ritase {unit}', fontsize=14)
+    ax.fill_between(data.index, data[f'{unit}'], color='#96b9d0', linewidth=1.1, alpha=0.1)
+    ax.plot(data.index, data[f'{unit}'], color='#fc8d62', linewidth=1.1)
--- functions/transform.py
+++ functions/transform.py
@@ -1,25 +1,24 @@
 import pandas as pd
 
-def get_ritase(path, unit):
+def get_ritase(path):
     """Get more organized and clean data from RITASE Raw Data
 
     Parameters:
     path (str): Location of RITASE Raw Data
-    unit (str): Unit Number. Either ex42 or ex44
 
     Returns:
     cleaned_data (dataframe): The cleaned RITASE pandas dataframe
     """
-    usecols = 'B:Z' if unit == 'ex42' else 'B, AA:AX'
-    raw_data = pd.read_excel(path, sheet_name=list(range(1, 13)), usecols=usecols, index_col=0, skiprows=2)
-    cleaned_data = pd.DataFrame(columns=['JAM', 'level_1', 0])
+    raw_data = pd.read_excel(path, sheet_name=list(range(1, 13)), index_col=0, header=None, skiprows=1)
+    cleaned_data = pd.DataFrame(columns=['LOADER', 'JAM', 1, 0])
     for i in range(1, 13):
-        cleaned_data = pd.concat([cleaned_data, raw_data.get(i).stack().reset_index()], ignore_index=True)
-    cleaned_data.rename(columns={'JAM': 'date', 'level_1': 'hour', 0: f'ritase_{unit}'}, inplace=True)
-    cleaned_data[f'ritase_{unit}'] = cleaned_data[f'ritase_{unit}'].astype('int')
-    cleaned_data['hour'] = cleaned_data['hour'].astype('float').astype('int')
+        temp_data = pd.DataFrame(raw_data.get(i).T[1:].values, columns=raw_data.get(i).T.iloc[0])
+        temp_data = temp_data.fillna(method="ffill").set_index(['LOADER', 'JAM']).stack().reset_index()
+        cleaned_data = pd.concat([cleaned_data, temp_data], ignore_index=True)
+    cleaned_data.columns = ['loader', 'hour', 'date', 'ritase']
+    cleaned_data[['hour', 'ritase']] = cleaned_data[['hour', 'ritase']].astype(int)
     cleaned_data['date'] = cleaned_data['date'] + pd.to_timedelta(cleaned_data['hour'], unit='h')
+    cleaned_data.sort_values('date', ignore_index=True, inplace=True)
     cleaned_data.drop(columns='hour', inplace=True)
-    if cleaned_data.isnull().sum().any():
-        cleaned_data.dropna(subset=['date'], inplace=True)
+    cleaned_data.dropna(subset=['date'], inplace=True)
     return cleaned_data
